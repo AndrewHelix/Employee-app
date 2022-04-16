7ff6d4dfe279a6e722fa846a87f8b3efc06d7f08
AndrewHelix <andreyshulga95@gmail.com>
2022-04-16 19:47:15 +0300

    add components

---
+++ src/components/search-panel/search-panel.js
@@ -0,0 +1,12 @@
+import './search-panel.css';
+
+const SearchPanel = () => {
+    return (
+        <input 
+          type="text"
+          className="search-text"
+          placeholder="Найти сотрудника"/>
+    )
+}
+
+export default SearchPanel;
